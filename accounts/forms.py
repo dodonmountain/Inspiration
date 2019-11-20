555181b79db22d3c5713c93145ec0c6935813f93
dodonmountain <lkh151515@gmail.com>
2019-11-20 12:10:26 +0900

    ini pjt 10

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'last_name', 'first_name', 'email')
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'class': 'm-5',
+                    }),
+        }
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('last_name', 'first_name',  'email')
+
