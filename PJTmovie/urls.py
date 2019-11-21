8e13361f27a232f6996ff49693ce33b335a26d66
taesu-park <tsoo1014@gmail.com>
2019-11-21 11:18:15 +0900

    Add Vue.js

--- PJTmovie/urls.py
+++ PJTmovie/urls.py
@@ -5,6 +5,7 @@ from accounts import urls
 
 
 urlpatterns = [
+    path('', include('indexes.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('movies/',include('movies.urls'))
