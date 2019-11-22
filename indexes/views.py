154741828f511c62621138bd806ac38fb5ba4a77
dodonmountain <lkh151515@gmail.com>
2019-11-22 17:21:19 +0900

    add carousel/INF scroll

--- indexes/views.py
+++ indexes/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from movies.models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    movies = Movie.objects.all()
+    movies = movies[:50]
+    context = {
+        'movies':movies
+    }
+    return render(request, 'index.html', context)
